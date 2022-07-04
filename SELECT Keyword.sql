d2951d74f06ed9e91d5858bc8cbedc87f3109108
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2022-07-05 00:10:15 +0100

    added sql scripts

---
+++ SELECT Keyword.sql
@@ -0,0 +1,30 @@
+/*
+SELECT Keyword Syntax
+To select certain columns from a specified table, use:
+SELECT column_1, column_2,... column_n
+FROM table_name;
+
+Or to select all columns from a specified table, use:
+SELECT *
+FROM table_name;
+*/
+
+SELECT 
+	first_name, last_name
+FROM
+	employees;
+
+SELECT
+	*
+FROM
+	employees;
+
+SELECT 
+    dept_no
+FROM
+    departments;
+
+SELECT 
+    *
+FROM
+    departments;
