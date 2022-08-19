9e35ca861d7f5a746eefb95a4659d5033b3f9f5e
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2022-08-19 02:47:43 +0100

    added sql scripts

--- Cross Join.sql
+++ Cross Join.sql
@@ -6,5 +6,25 @@ sets. It is particularly used when the tables in a database are not well connect
 
 # Syntax and Examples:
 
-select dm.*, d.*
-from depart
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm
+        CROSS JOIN
+    departments d
+ORDER BY dm.emp_no , d.dept_no;
+
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm,
+    departments d
+ORDER BY dm.emp_no , d.dept_no;
+
+SELECT 
+    dm.*, d.*
+FROM
+    dept_manager dm
+        JOIN
+    departments d
+ORDER BY dm.emp_no , d.dept_no;
