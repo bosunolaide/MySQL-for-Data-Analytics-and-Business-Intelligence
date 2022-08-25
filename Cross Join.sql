0eb59d048d72f08ae4c2dd731fd372a4cfebf248
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2022-08-25 02:14:55 +0100

    added sql scripts

--- Cross Join.sql
+++ Cross Join.sql
@@ -34,9 +34,51 @@ ORDER BY dm.emp_no , d.dept_no;
 SELECT 
     dm.*, d.*
 FROM
-    dept_manager dm
-        CROSS JOIN
     departments d
+        CROSS JOIN
+    dept_manager dm
 WHERE
     d.dept_no <> dm.dept_no
 ORDER BY dm.emp_no , d.dept_no;
+
+# Join more that two tables using a Cross Join.
+SELECT 
+    e.*, d.*
+FROM
+    departments d
+        CROSS JOIN
+    dept_manager dm
+		JOIN
+	employees e ON dm.emp_no = e.emp_no
+WHERE
+    d.dept_no <> dm.dept_no
+ORDER BY dm.emp_no , d.dept_no;
+
+/*Excercise: Use a CROSS JOIN to return a list with all possible combinations
+ between managers from the dept_manager table and department number 9.
+ */
+ 
+ SELECT 
+    dm.*, d.*
+FROM
+    departments d
+        CROSS JOIN
+    dept_manager dm
+WHERE
+    d.dept_no = 'd009'
+ORDER BY d.dept_name;
+
+/*
+Return a list with the first 10 employees with all the departments they can be assigned to.
+Hint: Don’t use LIMIT; use a WHERE clause.
+*/
+
+SELECT 
+    e.*, d.*
+FROM
+    employees e
+        CROSS JOIN
+    departments d
+WHERE
+    e.emp_no < 10011
+ORDER BY e.emp_no , d.dept_name; 
