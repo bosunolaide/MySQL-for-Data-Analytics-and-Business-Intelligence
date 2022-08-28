48f58d49afb5e384098a209c604feb8cf32c3769
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2022-08-28 02:05:56 +0100

    added sql scripts

--- Join Tips and Tricks.sql
+++ Join Tips and Tricks.sql
@@ -55,3 +55,14 @@ FROM
 GROUP BY d.dept_name
 HAVING average_salary > 60000
 ORDER BY average_salary DESC;
+
+# Exercise: How many male and how many female managers do we have in the ‘employees’ database?
+
+SELECT 
+    e.gender, COUNT(dm.emp_no) AS gender_count
+FROM
+    employees e
+        JOIN
+    dept_manager dm ON e.emp_no = dm.emp_no
+GROUP BY gender;
+
